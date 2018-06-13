6bfc743d9608ddfb516afa20008dc6c07d049c3b
Sarah Manning <sarahnmanning@gmail.com>
2018-06-13 18:27:35 -0400

    modified reminder index creating an array for the list of reminders and adding the date added to the list beside the item. Also looped the array to add multiple items to the list. Created the reminder.txt file.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const fs = require('fs'); // this tells node that we need this module (set of functions and attributes)
 console.log("Welcome to remindme");
-const reminders = "take a nap";
 const reminderFile = "reminders.txt";
 
-fs.writeFileSync(reminderFile, reminders);
+fs.unlinkSync(reminderFile);
+const reminders = [
+    "Go to the post office",             //0
+    "Put flea medicine on the animals",  //1
+    "Wash the sheets",                   //2
+    "Take a nap",                        //3
+    "Complete GDEV homework",            //4
+];
+
+const dates = [
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+];
+
+//for loop
+for(let i = 0; i < reminders.length; i += 1) {
+    const reminder = reminders[i];
+    const date = dates[i];
+    const line = `${reminder}|${date}\n`;
+    fs.appendFileSync(reminderFile, line);
+}
 console.log("Wrote all of the reminders to the file.");
