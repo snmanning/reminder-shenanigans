c90b9c8429d1927a5912a7fa0f27e1fbba1e738a
Sarah Manning <sarahnmanning@gmail.com>
2018-06-17 13:48:12 -0400

    Just made code changes and I was not able to keep up, so...this is stuff. Some stuff got changed and it's broke.

--- index.js
+++ index.js
@@ -35,16 +35,20 @@ else {
     help();
 }
 
-process.exit(0);
 console.log("Done");
+process.exit(0);
 
 
+// These will be hoisted into their callback when they are needed.
 function list() {
     console.log('Here are the things you need to do:');
+    const lines = fs.readFileSync(reminderFile, 'utf8').split(`\n`);
+    const parsedLines = lines.map(lines => line.split("|"));
     reminders.forEach((reminder, index) => {
-        const line = `   ${reminder}    Due: ${dates[index]}`;
+        const line = `   ${reminder}    Due: ${dates[index]}`; //data presented to a person can be spaced. It makes it easier to read
         console.log(line);
     });
+
 }
  
 function add() {
@@ -53,7 +57,7 @@ function add() {
 for(let i = 0; i < reminders.length; i += 1) {
     const reminder = reminders[i];
     const date = dates[i];
-    const line = `${reminder}|${date}\n`;
+    const line = `${reminder}|${date}\n`; //This data is written to a file, kept-stored-read by a computer. No spaces.
     fs.appendFileSync(reminderFile, line);
     }
 }
