1478e3aada562e9f7437b8a689c71673ca70fc7b
Sarah Manning <sarahnmanning@gmail.com>
2018-06-19 19:56:37 -0400

    created file for class module objects and two files for the objects. One for ReminderLlist and one for Reminder along with linking necessary files for running.

---
+++ models/ReminderList.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const Reminder = require('./Reminder');
+
+class ReminderList {
+    constructor(filepath) {
+        this.filepath = filepath;
+        const lines = fs.readFileSync(this.filepath, 'utf8').split(`\n`);
+        const parsedLine = lines.map(line => line.split("|"));
+        const humanFriendlyLines = parsedLine.map(parsedLine => {
+        const reminder = new Reminder(parsedLine[0], parsedLine[1]);
+        return reminder;
+            });
+        this.reminders = humanFriendlyLines;
+    }
+
+    add(reminder) {
+        this.reminders.push(reminder);
+    }
+
+    toFileOutput() {
+        const fileString = this.reminders.map((reminder) => reminder.toFileString());
+        const output = fileString.join(`\n`);
+        return output;
+    }
+
+    toConsole() {
+        const reminderString = this.reminders.map((reminder) => toString());
+        const output = reminderString.join(`\n`);
+        console.log(output);
+    }
+}
+
+module.exports = ReminderList;
