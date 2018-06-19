8911b37fc6756c912f1ab7a3b15fb6da64098892
Sarah Manning <sarahnmanning@gmail.com>
2018-06-19 19:52:57 -0400

    stripped down the code to skeleton. Moduled out the components to special files and folders

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const fs = require('fs'); // this tells node that we need this module (set of functions and attributes)
 console.log("Welcome to remindme");
-const reminders = "take a nap";
-const reminderFile = "reminders.txt";
 
-fs.writeFileSync(reminderFile, reminders);
-console.log("Wrote all of the reminders to the file.");
+const list = require('./commands/list');
+const add = require('./commands/add');
+const help = require('./commands/help');
+
+//list command, where all of the reminders are printed to scrceen
+//add command where we can add a reminder to the existing list
+const args = process.argv.slice(2);
+const subcommand = args[0];
+
+if(subcommand === 'list') {
+    list();
+    }
+else if(subcommand === 'add') {
+    const reminder = args[1];
+    add(reminder);
+    }
+else {
+    help();
+        }
+
+console.log("Ou fin! :)");
+process.exit(0);
