d228abba9b75694c17cabee1b37a76863a598680
Sarah Manning <sarahnmanning@gmail.com>
2018-06-19 19:54:26 -0400

    created commands folder with files for the main commands in the program; list, add, help. Each file contains only the code for their respective function and required linking files for running.

---
+++ commands/add.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const ReminderList = require('../models/ReminderList');
+const Reminder = require('../models/Reminder');
+const config = require('../config');
+const reminderFile = config.reminderFile;
+
+function add(newItem) {
+    console.log('Ammending a new item to your list...');
+    const reminderList = new ReminderList(reminderFile);
+    const reminder = new Reminder(newItem, new Date);
+    reminderList.add(reminder);
+    const output = reminderList.toFileOutput();
+    fs.unlinkSync(reminderFile);
+    fs.appendFileSync(reminderFile, output);
+}
+
+module.exports = add;
