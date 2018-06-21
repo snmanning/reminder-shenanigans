4f56bddd91407738e5182eae5ccd975b09718a79
Sarah Manning <sarahnmanning@gmail.com>
2018-06-20 20:26:45 -0400

    changes to align text into table-like format.

--- models/Reminder.js
+++ models/Reminder.js
@@ -1,16 +1,18 @@
 const chalk = require('chalk');
+const moment = require('moment');
+const { dateFormat, padding } = require('../config');
 
 class Reminder {
     constructor(inputText, inputDueDate) {
         this.text = inputText;
-        this.dueDate = inputDueDate;
+        this.dueDate = moment(inputDueDate);
     }
     toString() {
         const dueLabel = chalk.white.bgBlue('Due');
-        return `${chalk.yellow(this.text)}    ${dueLabel} ${chalk.magenta(this.dueDate)}`;
+        return ` - ${chalk.yellow(this.text).padEnd(padding)}${dueLabel} ${chalk.magenta(this.dueDate.format(dateFormat))}`;
     }
     toFileString() {
-        return `${this.text} | ${this.dueDate}`
+        return `${this.text} | ${this.dueDate.format(dateFormat)}`
     }
 }
 
