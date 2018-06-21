6db06cff0c7babcf991b360a5f393ae68c7358f9
Sarah Manning <sarahnmanning@gmail.com>
2018-06-20 20:49:00 -0400

    tried to move reminders.txt to home directory and point the file files to it. Broke.

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-module.exports.reminderFile = 'reminders.txt';
+const path = require('path');
+const os = require('os');
+module.exports.reminderFile = path.join(os.homedir(), '.reminders');
 module.exports.dateFormat = 'M/D/YYYY hh:mm a';
 module.exports.padding = 50;
--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const list = require('./commands/list');
 const add = require('./commands/add');
 const help = require('./commands/help');
 
+const fs = require('fs');
+const { reminderFile } = require('./config');
+
 //list command, where all of the reminders are printed to scrceen
 //add command where we can add a reminder to the existing list
+
+if(!fs.existsSync(reminderFile)) {
+    fs.writeFileSync(reminderFile, "");
+}
+
 const args = process.argv.slice(2);
 const subcommand = args[0];
 
